3fed63b8ba51b96e8f002e728ffb47d2f87d80b0
Jeta Bekteshi <jetabekteshi3@gmail.com>
2019-11-27 18:53:09 +0100

    add issue details

---
+++ src/app/issues-list/issue-details/issue-details.component.ts
@@ -0,0 +1,33 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {SearchIssueService} from '../../search-issue.service';
+
+@Component({
+  selector: 'app-issue-details',
+  templateUrl: './issue-details.component.html',
+  styleUrls: ['./issue-details.component.scss']
+})
+export class IssueDetailsComponent implements OnInit {
+  issueNumber: string;
+  issueDetails: any;
+
+  constructor(private activatedRoute: ActivatedRoute,
+              private searchIssueService: SearchIssueService) {
+  }
+
+  ngOnInit() {
+    this.setIssueNumberFromRoute();
+    this.getIssue();
+  }
+
+  setIssueNumberFromRoute() {
+    this.issueNumber = this.activatedRoute.snapshot.params.issueNumber;
+  }
+
+  getIssue() {
+    this.searchIssueService.getIssue(this.issueNumber).subscribe(response => {
+      this.issueDetails = response;
+    });
+  }
+
+}
